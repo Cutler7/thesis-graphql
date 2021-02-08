4fa502ffcaaebd55e46f12b17bf7dbeea5573bc7
Daniel Podolak <podolak9602@gmail.com>
2021-02-08 22:00:46 +0100

    merge frontend and backend project into a single repository

---
+++ backend/index.ts
@@ -0,0 +1,20 @@
+import {buildSchema} from "graphql";
+import {graphqlHTTP} from "express-graphql";
+import express from "express";
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = { hello: () => 'Hello world!' };
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(3000, () => console.log('Now browse to localhost:3000/graphql'));
